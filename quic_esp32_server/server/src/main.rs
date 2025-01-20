1cf3ab88063d3eca5220f5657753e21a69b9a146
dialMForMonkey <zz3qumk6i@relay.firefox.com>
2025-01-19 21:00:45 -0300

    esp32 ap mode

--- quic_esp32_server/server/src/main.rs
+++ quic_esp32_server/server/src/main.rs
@@ -1,16 +1,16 @@
 #![no_std]
 #![no_main]
 
+use core::str::FromStr;
+
 use esp_backtrace as _;
-use esp_hal::{ delay::Delay, gpio::{ Level, Output}, prelude::*, rng::Rng, timer::timg::TimerGroup};
-
-
+use esp_hal::{ delay::Delay, gpio::{ Level, Output}, main, rng::Rng, time::{self, Duration}, timer::timg::TimerGroup};
 extern crate alloc;
+use smoltcp::iface::{SocketSet, SocketStorage};
 
-
-#[entry]
+#[main]
 fn main() -> ! {
-    #[allow(unused)]
+    
     let peripherals = esp_hal::init(esp_hal::Config::default());
     
     let delay = Delay::new();
@@ -22,28 +22,72 @@ fn main() -> ! {
     
     let timg0 = TimerGroup::new(peripherals.TIMG1);
     
+    let mut random = Rng::new(peripherals.RNG);
+    let wifi_init = esp_wifi::init(timg0.timer0, random, peripherals.RADIO_CLK).expect("error init");
     
-    let wifi_init = esp_wifi::init(timg0.timer0, Rng::new(peripherals.RNG), peripherals.RADIO_CLK).expect("error init");
-    
+
+    esp_wifi::wifi_set_log_verbose();
+
     log::info!("wifi_controller.wifi {:?}", wifi_init.wifi());
     
-    let (wifi_device,mut wifi_controller) = esp_wifi::wifi::new_with_mode(&wifi_init, peripherals.WIFI, esp_wifi::wifi::WifiStaDevice ).expect("error start mode");
     
+
+     let (iface, wifi_device, mut wifi_controller) = esp_wifi::wifi::utils::create_network_interface
+     (&wifi_init,  peripherals.WIFI, esp_wifi::wifi::WifiApDevice)
+    .unwrap();
+    let now = || time::now().duration_since_epoch().to_millis();
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let socket_set = SocketSet::new(&mut socket_set_entries[..]);
+    
+    let mut stack = blocking_network_stack::Stack::new(iface, wifi_device, socket_set, now, random.random());
+    
+    stack
+        .set_iface_configuration(&blocking_network_stack::ipv4::Configuration::Client(
+            blocking_network_stack::ipv4::ClientConfiguration::Fixed(
+                blocking_network_stack::ipv4::ClientSettings {
+                    ip: blocking_network_stack::ipv4::Ipv4Addr::from(parse_ip("192.168.2.1")),
+                    subnet: blocking_network_stack::ipv4::Subnet {
+                        gateway: blocking_network_stack::ipv4::Ipv4Addr::from(parse_ip(
+                            "192.168.2.1",
+                        )),
+                        mask: blocking_network_stack::ipv4::Mask(24),
+                    },
+                    dns: Some( blocking_network_stack::ipv4::Ipv4Addr::from(parse_ip("1.1.1.1"))),
+                    secondary_dns: None,
+                },
+            ),
+        ))
+        .unwrap();
+
+
+    let config_ap: esp_wifi::wifi::AccessPointConfiguration = esp_wifi::wifi::AccessPointConfiguration {
+        ssid:  heapless::String::from_str("esp32_test").unwrap(),
+        ssid_hidden: false,
+        password: heapless::String::from_str("StrongPassword123!").unwrap(),
+        auth_method: esp_wifi::wifi::AuthMethod::WPA2WPA3Personal,
+        channel: 2,
+        ..Default::default()
+    };
+    
+
+    wifi_controller.set_configuration(  &esp_wifi::wifi::Configuration::AccessPoint(config_ap) ).expect("Error set config");
     let mut led =Output::new(peripherals.GPIO2, Level::High);
-    let mac =wifi_device.mac_address();
+    
     wifi_controller.start().expect("error when start");
-    log::info!(" mac {:?} ", mac);
     log::info!(" is_started {:?} ", wifi_controller.is_started());
+    log::info!(" is_ap_enabled {:?} ", wifi_controller.is_ap_enabled());
+   
     loop {
-        
-        let (wifi_list,_) = wifi_controller.scan_n::<32>().expect("error scan");
-       
-        wifi_list.iter().for_each(|a|{
-            log::info!("ssid {:?}", a.ssid);
-        });
-        
         led.toggle();
 
-        delay.delay(1000.millis());
+        delay.delay(Duration::millis(1000));
     }
+}
+
+fn parse_ip(ip: &str) -> [u8; 4] {
+    let mut result = [0u8; 4];
+    for (idx, octet) in ip.split(".").into_iter().enumerate() {
+        result[idx] = u8::from_str_radix(octet, 10).unwrap();
+    }
+    result
 }
